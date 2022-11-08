ba9f03c382c5bcac6401ef83558c21265596c6e2
ChristianL01 <locke.christian1@gmail.com>
2022-11-08 12:18:22 -0600

    add character-stat compenent

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharacterStatsComponent } from './character-stats/character-stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterStatsComponent
   ],
   imports: [
     BrowserModule,
