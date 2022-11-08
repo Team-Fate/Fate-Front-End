0c3142e5e5b4f7f925f506729541d49d398b40ee
ChristianL01 <locke.christian1@gmail.com>
2022-11-08 12:26:36 -0600

    add charCreation, charToken, charModel, charStats components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterStatsComponent } from './character-stats/character-stats.component';
+import { CharacterCreationComponent } from './character-creation/character-creation.component';
+import { CharacterTokenComponent } from './character-token/character-token.component';
+import { CharacterModelComponent } from './character-model/character-model.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterStatsComponent
+    CharacterStatsComponent,
+    CharacterCreationComponent,
+    CharacterTokenComponent,
+    CharacterModelComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/character-token/character-token.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-token',
+  templateUrl: './character-token.component.html',
+  styleUrls: ['./character-token.component.css']
+})
+export class CharacterTokenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
