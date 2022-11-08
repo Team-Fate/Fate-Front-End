273c186928dc0abf6e2409415cfdeb16389b3a4c
BrandonBishop84 <brandonnovember@gmail.com>
2022-11-08 14:22:21 -0800

    routes completed

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CharacterCreationComponent } from './character-creation/character-creation.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CombatComponent } from './combat/combat.component';
+import { MainComponent } from './main/main.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { StoryComponent } from './story/story.component';
 
 const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent }
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'app', component: AppComponent },
+  { path: 'character-list', component: CharacterListComponent },
+  { path: 'main', component: MainComponent },
+  { path: 'character-creation', component: CharacterCreationComponent },
+  { path: 'story', component: StoryComponent },
+  { path: 'combat', component: CombatComponent }
+
+
 ];
 
 @NgModule({
