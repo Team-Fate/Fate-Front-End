e42cc43ca68a7bab76be4b85f85a80727db2d502
BrandonBishop84 <brandonnovember@gmail.com>
2022-11-08 13:31:36 -0800

    app buttons functioning

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  constructor(private _router: Router) { }
+  continueFunction(event: MouseEvent) {
+    this._router.navigate(['sign-in']);
+
+}
+  narrateFunction(event: MouseEvent) {
+    this._router.navigate(['sign-up']);
+  }
 }
