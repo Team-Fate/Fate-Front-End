a36af2b3f3e7af3ca260444fcd48654d45abf856
thizzy824 <agpastora7@gmail.com>
2022-11-08 16:55:45 -0800

    establish connection to back-end, initial character GET request

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { CharacterListComponent } from './character-list/character-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'character-list', component: CharacterListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
