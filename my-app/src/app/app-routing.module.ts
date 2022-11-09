e77e90a08b4cc133d52d8c3a470e0a0f63a36b68
ChristianL01 <locke.christian1@gmail.com>
2022-11-09 03:12:34 -0600

    scaffold character-creation

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { CharacterListComponent } from './character-list/character-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterCreationComponent } from './character-creation/character-creation.component';
+
 
 const routes: Routes = [
-  { path: 'character-list', component: CharacterListComponent },
+  { path: 'character-creation', component: CharacterCreationComponent },
+  { path: 'character-list', component: CharacterListComponent }
 ];
 
 @NgModule({
