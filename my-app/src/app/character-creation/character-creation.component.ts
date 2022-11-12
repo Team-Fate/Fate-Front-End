a9461194c84da433518c94881aea369065ed8c45
ChristianL01 <locke.christian1@gmail.com>
2022-11-12 17:49:15 -0600

    character-creation component

--- my-app/src/app/character-creation/character-creation.component.ts
+++ my-app/src/app/character-creation/character-creation.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { FetchDataService } from './../services/fetch-data.service';
+
 
 @Component({
   selector: 'app-character-creation',
@@ -8,8 +11,23 @@ import { Router } from '@angular/router';
 })
 export class CharacterCreationComponent implements OnInit {
 
-  constructor(private router: Router,) { }
+  constructor(private _router: Router,) { }
+  form = new FormGroup({
+    characterName: new FormControl<string>('', Validators.required)
+  })
+    get characterName() {
+    return this.form.controls.characterName as FormControl;
+  }
 
+    submit() {
+    if (this.form.status == 'INVALID') {
+      return;
+    }
+    this._router.navigate(['character-list']);
+  }
+  biker = 'assets/images/Biker_idle.png'
+  cyborg = 'assets/images/Cyborg_idle.png'
+  punk = 'assets/images/Punk_idle.png'
   ngOnInit(): void {
   }
 
