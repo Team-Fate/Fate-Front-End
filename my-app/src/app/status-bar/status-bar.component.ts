a8f1f08645d18abb93cd69a686ed8baca4d2e6fc
vlados552 <vlados552@gmail.com>
2022-11-13 15:39:04 -0800

    Edit status bar component

--- my-app/src/app/status-bar/status-bar.component.ts
+++ my-app/src/app/status-bar/status-bar.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-status-bar',
   templateUrl: './status-bar.component.html',
-  styleUrls: ['./status-bar.component.css']
+  styleUrls: ['./status-bar.component.css'],
 })
 export class StatusBarComponent implements OnInit {
+  @Input() character: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
