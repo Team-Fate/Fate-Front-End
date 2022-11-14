3081ef37a7989875e86b023750675b2937cab3b4
vlados552 <vlados552@gmail.com>
2022-11-13 18:58:50 -0800

    Edit app component for display header

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-app';
-
-  constructor() { }
+  title = 'Your Fate';
+  displayHeader: any;
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.displayHeader = false;
+    this.router.events.subscribe((res) => {
+      if (this.router.url != '/') {
+        this.displayHeader = true;
+      } else {
+        this.displayHeader = false;
+      }
+    });
   }
-
- 
 }
